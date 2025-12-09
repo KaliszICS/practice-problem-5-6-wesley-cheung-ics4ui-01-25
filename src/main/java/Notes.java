b6946943fa1a2e44f58ea0eab4004de37a4ebda5
James Kalisz <james.kalisz@tdsb.on.ca>
2025-12-09 12:46:45 -0500

    update files

--- src/main/java/Notes.java
+++ src/main/java/Notes.java
@@ -1,6 +1,15 @@
-
 class Notes {
 	public static void main(String[] args) {
 
+		// Inheritance - is-A releationship
+
+		//Composition - has-A releationship
+
+		//Favor Composition over Inheritance.
+
+		//Is my code efficient
+		//Is my code easy to read
+		//Is my code easy to edit
+		
 	}
 }
